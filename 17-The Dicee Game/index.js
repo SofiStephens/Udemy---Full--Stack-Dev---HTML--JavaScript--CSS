9fbe16f5a4348b02ddc58307700a75e10e936bea
Sofía Inés Stephens Fracassi <122200446+SofiStephens@users.noreply.github.com>
2025-12-20 20:08:48 +0100

    .

--- 17-The Dicee Game/index.js
+++ 17-The Dicee Game/index.js
@@ -8,9 +8,9 @@ document.querySelector(".dice .img1").setAttribute("src", img1)
 document.querySelector(".dice .img2").setAttribute("src", img2)
 
 if(randomNumber1>randomNumber2){
-    document.querySelector("h1").textContent="Player 1 Wins!"
+    document.querySelector("h1").textContent="🚩Player 1 Wins!"
 }else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").textContent="Player 2 Wins!"
+    document.querySelector("h1").textContent="Player 2 Wins!🚩"
 }else {
     document.querySelector("h1").textContent= "Draw!"
 }
